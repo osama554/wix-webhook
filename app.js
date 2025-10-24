b5d0b29631be5cf3717ffd5e05b834fab7239ca7
osama554 <58435075+osama554@users.noreply.github.com>
2025-10-24 19:04:20 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -166,7 +166,11 @@ client.products.onProductChanged(async (event) => {
         const productId = event.data.productId;
         const accessToken = await getAccessToken(instanceId);
         const products = await getProduct(accessToken.token, productId);
-        console.log(products);
+        await App.findOneAndUpdate(
+            { instanceId, "products.productId": productId },
+            { $set: { "products.$": products.product } },
+            { new: true }
+        );
     } catch (error) {
         console.error("Error handling product change:", error);
     }
